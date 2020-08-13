ba4b81a7f78bb8b0c9094b8aad811fe6e1b1e96a
Michael Zadikowitsch <m.zadikowitsch@gmail.com>
2020-08-13 15:13:30 +0200

    react firstapp

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  icon: "fab fa-github",
+  title: "Github Finder",
+};
+
+Navbar.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Navbar;
